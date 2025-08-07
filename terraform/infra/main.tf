eb8d0f6ee1330e631ca5befaaff5f410ac2c5735
yvanniyonzima-ac <niyo0041@algonquinlive.com>
2025-08-07 18:36:28 -0400

    added github workflows and acr deployment

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -5,6 +5,15 @@ resource "azurerm_resource_group" "main_rg" {
   location = var.location
 }
 
+#` Create an Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "cst8918finalprojectacr"
+  resource_group_name = azurerm_resource_group.main_rg.name
+  location            = azurerm_resource_group.main_rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
 # Example usage of the network module (passing resource group)
 module "network" {
   source            = "./modules/network"
